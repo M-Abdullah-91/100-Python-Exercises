f1e57dc57dd560236b8e851402a46091c75319f8
Muhammad Abdullah <m.abdullah32196@gmail.com>
2024-05-17 15:01:44 +0500

    question1 done

--- 100 Python Exercises/question_1.py
+++ 100 Python Exercises/question_1.py
@@ -4,9 +4,10 @@ age1 = int(input("Enter Age 1: "))
 age2 = int(input("Enter Age 2: "))
 age3 = int(input("Enter Age 3: "))
 
-if age1>age2 and age1>age3:
-    print("Age 1 is Greater:")
-elif age2>age1 and age2>age3:
-    print("Age 2 is Greater:")
-if age3>age1 and age3>age2:
-    print("Age 3 is Greater:")
+if age1 > age2 and age1 > age3:
+    print("Age 1 is Greater")
+elif age2 > age3:
+    print("Age 2 is Greater")
+else:
+    print("Age 3 is Greater")
+
